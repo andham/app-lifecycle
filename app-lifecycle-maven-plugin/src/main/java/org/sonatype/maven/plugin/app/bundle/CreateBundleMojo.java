dcdb7c2e4bdab524e4d57f4bd675d69b0d89538e
John Casey <casey.john.d@gmail.com>
2009-07-21 22:16:58 +0000

    Update to provide licenses and valid site-deployment information for reports, etc.
    git-svn-id: file:///opt/svn/repositories/sonatype.org/plugins/trunk/app-lifecycle@272 c2a14038-686d-4adc-8740-a14e5738cb64

--- app-lifecycle-maven-plugin/src/main/java/org/sonatype/maven/plugin/app/bundle/CreateBundleMojo.java
+++ app-lifecycle-maven-plugin/src/main/java/org/sonatype/maven/plugin/app/bundle/CreateBundleMojo.java
@@ -1,20 +1,22 @@
-package org.sonatype.maven.plugin.app.bundle;
-
 /*
- * Copyright 2001-2005 The Apache Software Foundation.
+ * Sonatype Application Build Lifecycle
+ * Copyright (C) 2009 Sonatype, Inc.                                                                                                                          
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
  */
+package org.sonatype.maven.plugin.app.bundle;
 
 import org.apache.maven.execution.MavenSession;
 import org.apache.maven.plugin.AbstractMojo;
